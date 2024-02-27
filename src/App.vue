c8e5f16a942c5b81292ab49f2913dcc55928b57d
Tommy Dang <ledangt310@live.com>
2024-02-26 16:54:30 -0800

    made balace a variable, create total, expense, and income

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,60 @@
 <template>
   <Header />
   <div class="container">
-    <Balance />
+    <Balance :total="total" />
     <IncomeExpenses />
-    <TransactionList />
+    //passing 'transactions' array as a prop to TransactionList component
+    <TransactionList :transactions="transactions" />
     <AddTransaction />
   </div>
 </template>
 
-<script>
+//Using 'setup' in the script tag, is a quicker way of using the setup function.
+User does not have to explicity call the setup function and return the variable
+of the setup function.
+<script setup>
 import Header from './components/Header.vue';
 import Balance from './components/Balance.vue';
 import IncomeExpenses from './components/IncomeExpenses.vue';
 import TransactionList from './components/TransactionList.vue';
 import AddTransaction from './components/AddTransaction.vue';
 
-export default {
-  components: {
-    Header,
-    Balance,
-    IncomeExpenses,
-    TransactionList,
-    AddTransaction,
-  },
-};
+import { ref, computed } from 'vue';
+
+const transactions = ref([
+  { id: 1, text: 'Flower', amount: -19.99 },
+  { id: 2, text: 'Salary', amount: 299.97 },
+  { id: 3, text: 'Book', amount: -10 },
+  { id: 4, text: 'Camera', amount: 150 },
+]);
+
+//Get total
+const total = computed(() => {
+  return transactions.value.reduce((acc, transaction) => {
+    return acc + transaction.amount;
+  }, 0);
+});
+
+//Get income
+const income = computed(() => {
+  return transactions.value
+    .filter(() => transaction.amount > 0)
+    .reduce((acc, transaction) => {
+      return acc + transaction.amount;
+    }, 0)
+    .toFixed(2);
+});
+
+//Get expenses
+const expenses = computed(() => {
+  return transactions.value
+    .filter(() => transaction.amount < 0)
+    .reduce((acc, transaction) => {
+      return acc + transaction.amount;
+    }, 0)
+    .toFixed(2);
+});
 </script>
 
-<!-- Go to 28:40 
+<!-- Go to 39:00 
 https://www.youtube.com/watch?v=hNPwdOZ3qFU -->
--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -11,12 +11,13 @@
   </ul>
 </template>
 
-//Using 'setup' in the script tag, is a quicker way of using the setup function. User does not have to explicity call the setup function and return the variable of the setup function. 
 <script setup>
-    const transactions = [
-      { id: 1, text: 'Flower', amount: -19.99 },
-      { id: 2, text: 'Salary', amount: 299.97 },
-      { id: 3, text: 'Book', amount: -10 },
-      { id: 4, text: 'Camera', amount: -150 },
-    ];
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  transactions: {
+    type: Array,
+    required: true,
+  },
+});
 </script>
